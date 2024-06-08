aba628788d2d3cb159e48ade85bddfe8043b8dce
Renan Rudney <renanrudney@gmail.com>
2024-06-08 11:20:57 -0300

    feat: add email validation

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,28 @@
+import 'package:faker/faker.dart';
+import 'package:test/test.dart';
+
+import 'package:CleanFlutter/validation/validators/validators.dart';
+
+void main() {
+  EmailValidation sut;
+
+  setUp(() {
+    sut = EmailValidation('any_field');
+  });
+
+  test('Should return null if email is empty', () {
+    expect(sut.validate(''), null);
+  });
+
+  test('Should return null if email is null', () {
+    expect(sut.validate(null), null);
+  });
+
+  test('Should return null if email is valid', () {
+    expect(sut.validate(faker.internet.email()), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate(faker.internet.userName()), 'Campo inválido');
+  });
+}
